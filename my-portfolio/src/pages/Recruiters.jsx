303e9ff1b37839f913ccacc3467a5e5c30517c8d
AbhayKumar05 <abhaylagah01@gmail.com>
2025-08-23 23:30:28 +0530

    Almost finished the recruiter

--- my-portfolio/src/pages/Recruiters.jsx
+++ my-portfolio/src/pages/Recruiters.jsx
@@ -19,11 +19,13 @@ export default function Recruiters() {
         playsInline
       />
       <div className="recruiters-overlay">
-        <p>I’m Abhay Kumar, a Computer Engineering student at IADE Lisbon (graduating July 2025) with a strong foundation in full-stack web and Android development.
-         I enjoy building impactful software solutions—from responsive web apps with React and Spring Boot to mobile applications with Android SDK—and I’m always eager to learn and grow. I’ve gained hands-on experience through internships, hackathons, and projects, including co-developing Heartware, a donation platform that won 2nd place at Tech4Good 2025. Skilled in Java, Python, JavaScript, .NET Core, and PostgreSQL, I thrive in collaborative, fast-paced environments and am passionate about contributing to forward-thinking tech teams.</p>
+        <h1>ABHAY KUMAR</h1>
+        <p>I’m Abhay Kumar, a Computer Engineering graduate from IADE Lisbon, Portugal. With a strong foundation in full-stack web and Android development, I specialize in building impactful, user-friendly software solutions, from responsive web applications with React and Spring Boot to scalable backend systems and performance-focused Android apps. My experience spans professional internships, academic projects, and hackathons, where I’ve honed both my technical expertise and collaborative problem-solving skills.
+           During my internship at Codveda Technologies, I worked across frontend and backend development, gaining practical insights into delivering reliable and responsive web experiences. I also had the opportunity to co-develop Heartware, a donation platform built with React, .NET Core, and PostgreSQL, which secured 2nd place in the Apps Category and an Honorable Mention at Tech4Good 2025. Experiences like these have not only sharpened my technical skills but also strengthened my adaptability, creativity, and ability to thrive in fast-paced, team-driven environments.
+           I’m passionate about using technology to create meaningful impact and continuously expanding my knowledge in areas like software architecture, API development, and system security. Moving forward, I’m eager to contribute to forward-thinking teams, tackle complex challenges, and continue building solutions that blend innovation with real-world value.</p>
         <div className="recruiters-buttons">
-          <button onClick={handleShowCV}>Show My CV</button>
-          <button onClick={handleLinkedIn}>LinkedIn</button>
+          <button onClick={handleShowCV}>▶ Resume</button>
+          <button onClick={handleLinkedIn}>ⓘ LinkedIn</button>
         </div>
       </div>
     </div>
