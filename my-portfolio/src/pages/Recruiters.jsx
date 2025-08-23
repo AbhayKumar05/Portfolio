7b2254fe02d150c353eaf26d72dffe4e98f5bfe4
AbhayKumar05 <abhaylagah01@gmail.com>
2025-08-23 23:04:04 +0530

    recruiter

--- my-portfolio/src/pages/Recruiters.jsx
+++ my-portfolio/src/pages/Recruiters.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import "./Recruiters.css";
 
 export default function Recruiters() {
+  const handleShowCV = () => {
+    window.open("/cv.pdf", "_blank");
+  };
+  const handleLinkedIn = () => {
+    window.open("https://www.linkedin.com/in/abhay-kumar-13831b248", "_blank");
+  };
   return (
     <div className="recruiters-bg-container">
       <video
@@ -13,12 +19,11 @@ export default function Recruiters() {
         playsInline
       />
       <div className="recruiters-overlay">
-        <h1>Welcome, Recruiter!</h1>
-        <p>I’m Abhay Kumar, a Computer Engineering student at IADE Lisbon (graduating July 2025) with a strong foundation in full-stack web and Android development. </p>
-        <p> I enjoy building impactful software solutions—from responsive web apps with React and Spring Boot to mobile applications with Android SDK—and I’m always eager to learn and grow. I’ve gained hands-on experience through internships, hackathons, and projects, including co-developing Heartware, a donation platform that won 2nd place at Tech4Good 2025. Skilled in Java, Python, JavaScript, .NET Core, and PostgreSQL, I thrive in collaborative, fast-paced environments and am passionate about contributing to forward-thinking tech teams.</p>
+        <p>I’m Abhay Kumar, a Computer Engineering student at IADE Lisbon (graduating July 2025) with a strong foundation in full-stack web and Android development.
+         I enjoy building impactful software solutions—from responsive web apps with React and Spring Boot to mobile applications with Android SDK—and I’m always eager to learn and grow. I’ve gained hands-on experience through internships, hackathons, and projects, including co-developing Heartware, a donation platform that won 2nd place at Tech4Good 2025. Skilled in Java, Python, JavaScript, .NET Core, and PostgreSQL, I thrive in collaborative, fast-paced environments and am passionate about contributing to forward-thinking tech teams.</p>
         <div className="recruiters-buttons">
-          <button>View Projects</button>
-          <button>Contact Me</button>
+          <button onClick={handleShowCV}>Show My CV</button>
+          <button onClick={handleLinkedIn}>LinkedIn</button>
         </div>
       </div>
     </div>
