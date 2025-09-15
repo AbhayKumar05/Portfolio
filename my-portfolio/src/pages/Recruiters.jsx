a02b19c8787b236db2893e84c7065dd7c338e3a9
AbhayKumar05 <abhaylagah01@gmail.com>
2025-09-15 22:28:17 +0100

    working on intro text

--- my-portfolio/src/pages/Recruiters.jsx
+++ my-portfolio/src/pages/Recruiters.jsx
@@ -21,9 +21,11 @@ export default function Recruiters() {
         />
         <div className="recruiters-overlay">
           <h1>ABHAY KUMAR</h1>
-          <p>I’m Abhay Kumar, a Computer Engineering graduate from IADE Lisbon, Portugal. With a strong foundation in full-stack web and Android development, I specialize in building impactful, user-friendly software solutions, from responsive web applications with React and Spring Boot to scalable backend systems and performance-focused Android apps. My experience spans professional internships, academic projects, and hackathons, where I’ve honed both my technical expertise and collaborative problem-solving skills.
-            During my internship at Codveda Technologies, I worked across frontend and backend development, gaining practical insights into delivering reliable and responsive web experiences. I also had the opportunity to co-develop Heartware, a donation platform built with React, .NET Core, and PostgreSQL, which secured 2nd place in the Apps Category and an Honorable Mention at Tech4Good 2025. Experiences like these have not only sharpened my technical skills but also strengthened my adaptability, creativity, and ability to thrive in fast-paced, team-driven environments.
-            I’m passionate about using technology to create meaningful impact and continuously expanding my knowledge in areas like software architecture, API development, and system security. Moving forward, I’m eager to contribute to forward-thinking teams, tackle complex challenges, and continue building solutions that blend innovation with real-world value.</p>
+           <p> Computer Engineering graduate from IADE Lisbon, Portugal.</p>
+           <p> Skilled in React, Spring Boot, Android, and APIs.</p>
+           <p> Experience in internships, projects, and hackathons.</p>
+           <p>Co-developed award-winning donation platform Heartware.</p>
+           <p> Passionate about impactful, scalable, user-focused solutions.</p>
           <div className="recruiters-buttons">
             <button onClick={handleShowCV}>▶ Resume</button>
             <button onClick={handleLinkedIn}>ⓘ LinkedIn</button>
