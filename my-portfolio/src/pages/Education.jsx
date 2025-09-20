3b61840d4012f3f775dea507c350e858147f7355
AbhayKumar05 <abhaylagah01@gmail.com>
2025-09-20 23:41:54 +0100

    Education page - done

--- my-portfolio/src/pages/Education.jsx
+++ my-portfolio/src/pages/Education.jsx
@@ -2,34 +2,44 @@ import React from "react";
 import "./Education.css";
 import Navbar from "../components/Navbar"; 
 
-const projectData = [
+const educationData = [
   {
     id: 1,
-    title: "Bachelor's degree in Computer Engineering",
+    title: "Bachelor's Degree in Computer Engineering",
     description:
-    "I graduated with a solid foundation in Computer Engineering, where I worked on projects that connected both software and hardware, gradually increasing in complexity. Throughout my degree, I built a portfolio that reflects not only my technical skills but also my ability to manage projects and collaborate in teams. The program was closely aligned with industry trends like Artificial Intelligence, IoT, and intelligent systems, giving me strong scientific and technical knowledge as well as hands-on experience with physical computing and IoT technologies. This comprehensive education has prepared me to tackle real-world challenges in the tech industry.",
+      "I graduated with a solid foundation in Computer Engineering, where I worked on projects that connected both software and hardware, gradually increasing in complexity. Throughout my degree, I built a portfolio that reflects not only my technical skills but also my ability to manage projects and collaborate in teams. The program was closely aligned with industry trends like Artificial Intelligence, IoT, and intelligent systems, giving me strong scientific and technical knowledge as well as hands-on experience with physical computing and IoT technologies. This comprehensive education has prepared me to tackle real-world challenges in the tech industry.",
     image: "profiles/iade.png",
+  },
+  {
+    id: 2,
+    title: "AWS Training & Certification",
+    description:
+      "Completion Certificate: AWS SimuLearn – Cloud Computing Essentials. Completed September 20, 2025. Awarded to Abhay Kumar. Verified by Michelle Vaz, Director of AWS Training & Certification.",
+    image: "profiles/aws.png", 
+  },
+  {
+    id: 3,
+    title: "Spring Boot Essentials 2",
+    description:
+      "Completed the 'Spring Boot Essentials 2' course, covering full lifecycle Spring Boot development, from project setup to API security, monitoring, Docker integration, and deployment via Docker Compose. Hands-on experience in scalable backend development and RESTful API best practices.",
+    image: "profiles/springbbote.png", 
   },
 ];
 
-export default function Projects() {
+export default function Education() {
   return (
     <>
       <Navbar />
-
-      <div className="projects-content">
-        <div className="recruiters-project">
-          {projectData.map((proj) => (
-            <div className="education-card" key={proj.id}>
-              {/* Top Half Image */}
+      <div className="education-page">
+        <div className="education-list">
+          {educationData.map((edu) => (
+            <div className="education-card" key={edu.id}>
               <div className="education-image">
-                <img src={proj.image} alt={proj.title} />
+                <img src={edu.image} alt={edu.title} />
               </div>
-
-              {/* Bottom Half Text */}
-              <div className="project-info">
-                <h3>{proj.title}</h3>
-                <p>{proj.description}</p>
+              <div className="education-info">
+                <h3>{edu.title}</h3>
+                <p>{edu.description}</p>
               </div>
             </div>
           ))}
