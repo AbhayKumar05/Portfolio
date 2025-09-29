59957e1f0cd71d92f420e0da73660d709c7b04de
AbhayKumar05 <abhaylagah01@gmail.com>
2025-09-29 19:34:32 +0100

    just added image for now

--- my-portfolio/src/pages/Adventurer.jsx
+++ my-portfolio/src/pages/Adventurer.jsx
@@ -22,9 +22,9 @@ export default function Adventurer() {
       <Navbar />
 
       <div className="recruiters-bg-container">
-        <img
-          className="adventurer-bg-video" id="iade"
-          src={`${import.meta.env.BASE_URL}profiles/iade-dark.png`}
+        <video
+          className="adventurer-bg-video"
+          src={`${import.meta.env.BASE_URL}profiles/advv.mp4`}
           autoPlay
           loop
           muted
